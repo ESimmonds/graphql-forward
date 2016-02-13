2152597268dd2cb9aa56e23e3497f9df40420694
Samer Buna <samer.buna@gmail.com>
2016-02-13 11:38:37 -0800

    hello graphql

---
+++ data/schema.js
@@ -0,0 +1,22 @@
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} from 'graphql';
+
+var schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        }
+      }
+    }
+  }),
+  // mutation: ...
+});
+
+export default schema;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-var express = require('express');
+import graphqlHTTP from "express-graphql";
+import schema from "./data/schema";
+import express from "express";
+
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.use('/graphql', graphqlHTTP({ schema, graphiql: false }) )
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
